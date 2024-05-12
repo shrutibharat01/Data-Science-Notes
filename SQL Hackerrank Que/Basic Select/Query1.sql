b3228be3ea4e0168ae265802d21397721942057a
Shruti_Bharat <bharatshruti02@gmail.com>
2024-05-13 01:40:55 +0530

    Update Query1.sql

--- SQL Hackerrank Que/Basic Select/Query1.sql
+++ SQL Hackerrank Que/Basic Select/Query1.sql
@@ -1,7 +1,7 @@
-### Query: All columns for all American cities in the CITY table with populations larger than 100,000
+-- Query: All columns for all American cities in the CITY table with populations larger than 100,000
 
-select * from city
-where population > 100000 and countrycode = 'USA';
+SELECT * FROM city
+WHERE population > 100000 AND countrycode = 'USA';
 
 
 
